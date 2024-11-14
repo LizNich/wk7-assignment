382bf7da1deb87adcd5d386b9eea3ec86e4b69db
LizNich <nicholsonliz@hotmail.com>
2024-11-14 09:37:43 +0000

    Set up Add Server

---
+++ server/server.js
@@ -0,0 +1,39 @@
+// Set up Nodes
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
+
+// Set up Server
+const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+// Set up DB connection - link in the .env.
+const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+
+app.get("/", function (request, response) {
+  response.json("no diggity no route route");
+});
+
+// Link to db
+app.get("/cakes", function (request, response) {
+  response.json("Here be cakes!");
+});
+
+//here need to add the SQL query from supabase
+app.get("/cakes", function (request, response) {
+const result = await db.query('
+  SELECT 
+  ...app
+  ');
+  response.json(result);
+});
+
+
+
+
+
+
+app.listen(8080, () => console.log("App is running on port 8080"));
